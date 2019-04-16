2cfc936d4a13a00a5cc37a90b75d9c54a8776d2b
youeryuango <artist.phper@gmail.com>
2019-04-16 11:25:56 +0800

    Golang的处理运行时错误

--- labs/funcLab07.go
+++ labs/funcLab07.go
@@ -11,6 +11,19 @@ import (
 
 func main() {
 	fmt.Println(div(1, 0))
+
+	var e error
+	// 创建一个错误实例 包含文件名和行号
+	e = newParseError("funcLab06.go", 1)
+	// 通过error接口查看错误描述
+	fmt.Println(e.Error())
+	// 根据接口具体的类型，获取详细错误信息
+	switch detail := e.(type) {
+	case *ParseError:
+		fmt.Printf("Filename: %s Line: %d\n", detail.Filename, detail.Line)
+	default:
+		fmt.Println("other error")
+	}
 }
 // 定义除数为0的错误
 var errDivisionByZero = errors.New("division by zero")
@@ -21,4 +34,22 @@ func div(dividend, divisor int) (int, error) {
 	}
 	// 正常计算返回空错误
 	return dividend / divisor, nil
+}
+
+// 在解析中使用自定义错误
+
+// 声明一个解析错误
+type ParseError struct {
+	Filename string
+	Line     int
+}
+
+// 实现error接口，返回错误描述
+func (e *ParseError) Error() string  {
+	return fmt.Sprintf("%s:%d", e.Filename, e.Line)
+}
+
+// 创建一些解析错误
+func newParseError(filename string, line int) error {
+	return &ParseError{filename, line}
 }

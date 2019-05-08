783f9e2d30915542ce54e2ddf2ab0e8c666383b2
youeryuango <artist.phper@gmail.com>
2019-05-08 10:47:00 +0800

    关于context包的使用

--- demo/context.go
+++ demo/context.go
@@ -4,27 +4,40 @@ import (
 	"net/http"
 	"github.com/gorilla/context"
 	"strconv"
+	"sync"
 )
-// 在一个 request 的处理期间，保存一些值
+
+/* desc
+** context 包可以在一个 request 的处理期间，保存一些值
+** func Clear(r *http.Request)
+** func ClearHandler(h http.Handler) http.Handler
+** func Delete(r *http.Request, key interface{})
+** func Get(r *http.Request, key interface{}) interface{}
+** func GetAll(r *http.Request) map[interface{}]interface{}
+** func GetAllOk(r *http.Request) (map[interface{}]interface{}, bool)
+** func GetOk(r *http.Request, key interface{}) (interface{}, bool)
+** func Purge(maxAge int) int
+** func Set(r *http.Request, key, val interface{})
+*/
 func main() {
 	// 启动一个Web服务
-	http.Handle("/", http.HandlerFunc(myHandle))
+	http.Handle("/", http.HandlerFunc(MyHandler))
 	http.ListenAndServe(":9999", nil)
 }
-
-func myHandle(rw http.ResponseWriter, r *http.Request)  {
-	// 模拟为Request附加值
-	context.Set(r, "user", "张大炮")
-	context.Set(r, "age", 23)
-	context.Set(r, "job", "programmer")
-	context.Set(r, "gender", "man")
-	// 这个模拟一个方法或者函数的调用，大部分情况下可能不在一个包里
-	doHandle(rw, r)
-}
-func doHandle(rw http.ResponseWriter, r *http.Request)  {
-	user := context.Get(r, "user").(string)
-	age  := context.Get(r, "age").(int)
-	job  := context.Get(r, "job").(string)
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("this user is " + user + ", age is " + strconv.Itoa(age) + " job is " + job))
+var mutex sync.RWMutex
+const MyKey int = 0
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+	// 只允许一个Go协程 避免竞态条件
+	mutex.Lock()
+	context.Set(r, MyKey, "bar")
+	// 删除MyKey
+	context.Delete(r, MyKey)
+	if  rv := context.Get(r, MyKey); rv != nil{
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("The MyKey " + strconv.Itoa(MyKey) + " is " + rv.(string)))
+	}else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("The MyKey " + strconv.Itoa(MyKey) + " is nothing"))
+	}
+	mutex.Unlock()
 }

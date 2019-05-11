467b5ff95ea25f61bda677999a06c5c9021c3245
youeryuango <artist.phper@gmail.com>
2019-05-11 18:05:12 +0800

    爬虫抓取妹子图网站写真图片

---
+++ spider/girls.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
+// 功能描述：使用Golang编写爬虫，抓取“妹子图”网站部分图片
+// 作者声明：本篇代码只用作学习与交流，不可作商业用途
+func main() {
+	GrabIndexPage()
+}
+// 抓取首页信息
+func GrabIndexPage()  {
+	url := "http://www.mmmjpg.com"
+	document, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	document.Find(".pic li").Each(func(i int, selection *goquery.Selection) {
+		// 缩略图
+		//smallImg, _ := selection.Find("img").Attr("src")
+		// 详情页的链接后半部分
+		pageHref, _ :=  selection.Find("a").Attr("href")
+		detailPageLink := url + pageHref
+		GrabDetailPage(detailPageLink)
+	})
+}
+// 获取详情页
+func GrabDetailPage(link string)  {
+	document,err := goquery.NewDocument(link)
+	if err != nil {
+		fmt.Println(err)
+	}
+	imgSrc,_ := document.Find(".content img").Attr("src")
+	series,_ := document.Find(".content img").Attr("alt")
+	// 结果如：http://img.hi0590.com/1453/1.jpg
+	// 根据页数查共有多少张图片
+	pageNumStr := document.Find(".page a").Eq(-2).Text()
+	pageNum, _ := strconv.Atoi(pageNumStr)
+	// 截取http://img.hi0590.com/1453/
+	for i := 1; i <= pageNum; i++ {
+		filename := strconv.Itoa(i) + ".jpg"
+		tmpSrc := imgSrc[ : len(imgSrc) - 5] + filename
+		DownloadImage(tmpSrc, series, filename)
+	}
+}
+// 下载图片
+func DownloadImage(src, series, filename string)  {
+	dir := "spider/images/www.mmmjpg.com/" + series + "/"
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req, err := http.NewRequest("Get", src, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	resp, _ := http.DefaultClient.Do(req)
+	file, err := os.OpenFile(dir + filename, os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("正在抓取图片链接为:" + src)
+	if _, err := io.Copy(file, resp.Body);err != nil {
+		panic("文件保存失败: " + fmt.Sprintf("%v", err))
+	}
+	fmt.Println("抓取" + src + "结束")
+}
---
+++ spider/girls.md
@@ -0,0 +1,41 @@
+#### 美女写真网站(http://www.mmmjpg.com)html分析
+##### 缩略图html分析
+```
+<div class="pic">
+    <li>
+        <a href="/mm/1434" target="_blank">
+            <img src="http://img.hi0590.com/p/1434.jpg" width="220" height="330" alt="高颜值美女令人迷醉露胸诱惑令人躁动">
+        </a>
+        <span class="title">
+            <a href="/mm/1434" target="_blank">
+                高颜值美女令人迷醉露胸诱惑令人躁动
+            </a>
+        </span>
+        <span>04-22 发布
+        </span>
+        <span class="view">浏览(12095)
+        </span>
+    </li>
+</div>
+```
+##### 图片链接对比
+```
+// 缩略图 http://img.hi0590.com/p/1434.jpg
+// 大图   http://img.hi0590.com/1434/1.jpg
+```
+##### 详情页页码分析
+```
+<div class="page" id="page">
+    <a href="/mm/1415" class="ch pre">上一篇</a>
+    <em>1</em>
+    <a href="/mm/1414/2">2</a>
+    <a href="/mm/1414/3">3</a>
+    <a href="/mm/1414/4">4</a>
+    <a href="/mm/1414/5">5</a>
+    <a href="/mm/1414/6">6</a>
+    <span>...</span>
+    <a href="/mm/1414/45">45</a>
+    <em class="ch all" id="opic" onclick="openall(1);">全部图片</em>
+    <a href="/mm/1414/2" class="ch next">下一页</a>
+</div>
+```
